b5be416b1e1df3e8f352ca88115b5d009daeb3a6
I-Love-Bugs <54188092+zohirben@users.noreply.github.com>
2022-10-10 15:56:55 +0100

    Add files via upload

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -8,7 +8,7 @@ char    *ft_strjoin(char const *s1, char const *s2)
 
     if (!s1 || !s2)
         return (0);
-    str = (char *) malloc(strlen(s1) + strlen(s2) + 1);
+    str = (char *) malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
     if (!str)
         return (0);
     i = 0;
