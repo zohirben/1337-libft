ad4e4e0fd42a3d6958bd1f13d0d9af45b241fec9
I-Love-Bugs <54188092+zohirben@users.noreply.github.com>
2022-10-20 23:50:43 +0100

    adding strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,3 +1,41 @@
-// #include "libft.h"
+#include "libft.h"
 
-// char    *ft_strmapi()
+char ft_crypt(unsigned int key, char c)
+{
+    return (c+=key);
+}
+
+char ft_decrypt(unsigned int key, char c)
+{
+    return (c-=key);
+}
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    int i;
+    int len;
+    char    *str;
+
+    i = 0;
+    len = strlen(s);
+    str = (char *)malloc((len + 1));
+    if (!str)
+        return (0);
+    while (i < len)
+    {
+        str[i] = (*f)(7, s[i]);
+        i++;
+    }
+    str[i] = '\0';
+    return (str);
+}
+
+// int main()
+// {
+//     const char *s = "Hello World";
+//     char *str;
+//     str = ft_strmapi(s, ft_crypt);
+//     printf("%s\n", str);
+//     str = ft_strmapi(str, ft_decrypt);
+//     printf("%s\n", str);
+// }
